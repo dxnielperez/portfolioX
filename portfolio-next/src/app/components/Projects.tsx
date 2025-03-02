9cb8ed7594d95be573b3af6765d32710339faf69
Daniel <danielperezf98@gmail.com>
2025-03-01 20:23:14 -0800

    removed lazy loading from project imgages

--- portfolio-next/src/app/components/Projects.tsx
+++ portfolio-next/src/app/components/Projects.tsx
@@ -195,7 +195,6 @@ export function Projects() {
               <Image
                 width={800}
                 height={800}
-                loading="lazy"
                 src={projects[selectedProjectIndex].images[selectedImageIndex]}
                 className=" max-w-[420px] sm:max-w-[520px] md:max-w-[620px] lg:max-w-[720px] max-h-[90vh]"
                 alt={`${selectedProject?.title} project images in modal`}
